97a7c2d1ae8856148182f6ea6c8c34efc52c0cd4
Fadhlan Hawali <fadhlan.hawali@mail.ugm.ac.id>
2020-06-27 21:10:16 +0700

    add hamburger animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click',function(){
+    console.log('open hamburger')
+    if(header.classList.contains('open')){ //Close Hamburger
+      header.classList.remove('open');
+      overlay.classList.remove('fade-in');
+      overlay.classList.add('fade-out');
+    }else{ //Open Hamburger
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+    }
+});
